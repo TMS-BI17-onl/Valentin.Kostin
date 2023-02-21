23d625554629ee808472c4cb8228c13e435fd912
valentin.kostin <viking_9797@mail.ru>
2023-02-21 20:49:30 +0300

    PW2 added

--- PW2/PW2.sql
+++ PW2/PW2.sql
@@ -12,10 +12,20 @@ SELECT NationalIDNumber, BirthDate, YEAR(GETDATE()) - YEAR(BirthDate) AS Age,
 		WHEN YEAR(GETDATE()) - YEAR(BirthDate)BETWEEN 21 AND 59 THEN 'Adults'
 		WHEN YEAR(GETDATE()) - YEAR(BirthDate) BETWEEN 60 AND 75 THEN 'Elderly'
 		WHEN YEAR(GETDATE()) - YEAR(BirthDate) BETWEEN 76 AND 90 THEN 'Senile'
-		ELSE 'Othhers'
+		ELSE 'Others'
 		END AS Age
-FROM HumanResources.Employee
-;
+FROM HumanResources.Employee;
+
+/* doesn't work(
+SELECT BusinessEntityID,
+(datediff(year, HumanResources.Employee.BirthDate, getdate()) - case 
+ WHEN datepart(dayofyear, HumanResources.Employee.BirthDate) >= 17 AND datepart(dayofyear, HumanResources.Employee.BirthDate) <= 20 THEN 'Adolescence'
+ WHEN datepart(dayofyear, HumanResources.Employee.BirthDate) >= 21 AND datepart(dayofyear, HumanResources.Employee.BirthDate) <= 59 THEN 'Adults'
+ WHEN datepart(dayofyear, HumanResources.Employee.BirthDate) >= 60 AND datepart(dayofyear, HumanResources.Employee.BirthDate) <= 75 THEN 'Elderly'
+ WHEN datepart(dayofyear, HumanResources.Employee.BirthDate) >= 76 AND datepart(dayofyear, HumanResources.Employee.BirthDate) <= 90 THEN 'Senile'
+ ELSE 'N/S'
+ END) AS Age
+FROM HumanResources.Employee; */
 
 --Exercise 3
 SELECT Name, Color, StandardCost
